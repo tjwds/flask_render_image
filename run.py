ecb8b522493d378a0fd6e3b6d11269972f950c07
Joe Woods <woodsja@vcu.edu>
2018-06-10 16:23:25 -0400

    better rendering without disk race condition

--- run.py
+++ run.py
@@ -1,14 +1,24 @@
-from flask import Flask
+from flask import Flask, send_file
 from generate import generate_art
-import os
+import io
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    print(os.path.abspath(os.curdir))
+    return '<img src="./image.jpg" />'
+
+@app.route('/image.jpg')
+def render_image():
     image = generate_art()
-    image.save('static/images/render.jpg')
-    return '<img src="./static/images/render.jpg" />'
+    b = io.BytesIO()
+    image.save(b, 'JPEG')
+    b.seek(0)
+    return send_file(
+        b,
+        mimetype='image/jpeg',
+        as_attachment=True,
+        attachment_filename='image.jpg')
+
 
 app.run(debug=True)
